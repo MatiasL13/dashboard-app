ec287f5dc4b733998ea93109fec5c9ff312acf4b
MatiasL13 <matias.l.sanchez@gmail.com>
2020-07-24 03:20:27 -0300

    init y estilo de pagina principal

---
+++ pages/exchange.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-alert type="warning">Wallet is old!</v-alert>
+        <v-row justify="center">
+          <v-col cols="5">
+            <Block />
+          </v-col>
+          <v-col cols="5">
+            <Block />
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
